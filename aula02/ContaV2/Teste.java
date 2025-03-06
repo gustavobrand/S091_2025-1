a49dfda6352b0153129a21ae8d7a6335ff9bb5ee
Gustavo Brand <41551014+gustavobrand@users.noreply.github.com>
2025-03-05 22:15:57 -0300

    Add files via upload

--- aula02/ContaV2/Teste.java
+++ aula02/ContaV2/Teste.java
@@ -3,7 +3,7 @@ package aula02.ContaV2;
 public class Teste {
 
 	public static void main(String[] args) {
-		ContaCorrente cc = new ContaCorrente(0, 1000, "Joao", 10);
+		ContaCorrente cc = new ContaCorrente(0, "Joao", 1000, 10);
 		ContaPoupanca cp = new ContaPoupanca(0, "Maria", 1);
 		cc.depositar(100);
 		System.out.println(cc);
